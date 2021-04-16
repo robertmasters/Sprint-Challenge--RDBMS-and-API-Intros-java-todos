c2266d5c938fe3c7cef685a74ef85e28156d86b1
Robert-Rodriguez <robertmasters@hotmail.com>
2021-04-16 05:57:55 -0700

    need to test still

--- src/main/java/com/lambdaschool/todos/TodosApplication.java
+++ src/main/java/com/lambdaschool/todos/TodosApplication.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 /**
  * Main class to start the application.
  */
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class TodosApplication
